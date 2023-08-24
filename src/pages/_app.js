aba23f32c2eefe31a776f61c0f3697482b32a3e4
himanshu <himanshu01.dev@gmail.com>
2023-08-24 13:50:28 +0530

    vercel analytics :added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
