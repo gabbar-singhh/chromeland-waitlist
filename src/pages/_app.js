39cd16e866e69396db69794cd4d8eab160ec9675
himanshu <himanshu01.dev@gmail.com>
2023-09-16 15:53:06 +0530

    OS CHANGED

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,20 +5,6 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>JOIN THE WAITLIST!</title>
-        <meta name="description" content="Homepage you deserve!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* GOOGLE FONTS */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>JOIN THE WAITLIST!</title>
+        <meta name="description" content="Homepage you deserve!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+
+        {/* GOOGLE FONTS */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
